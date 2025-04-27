dba299e41be5e9561c7a09903b407cd0ea625a01
Зайнуллин Айнур Хакимович <ZaynullinAKh@alrosa.ru>
2025-04-27 18:52:18 +0500

    Начальное накидывание структуры проекта

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,11 +1,60 @@
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Welcome } from '../components/Welcome/Welcome';
+import { useDisclosure } from '@mantine/hooks';
+import { AppShell, Button, Burger, Divider, Group } from '@mantine/core';
+import { Calendar } from '@mantine/dates';
+
+import FullCalendar from '@fullcalendar/react';
+// import dayGridPlugin from '@fullcalendar/daygrid'
+import resourceTimeGridPlugin from '@fullcalendar/resource-timegrid'; // a plugin!
+
+import treatment from './treatment';
+import events from './events';
 
 export function HomePage() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-    <>
-      <Welcome />
-      <ColorSchemeToggle />
-    </>
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+      }}
+      padding="md"
+    >
+      <AppShell.Header >
+        <Group align="center">
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            size="sm"
+          />
+          <Button>Добавить лечение</Button>
+          <Button>Добавить врача</Button>
+        </Group>
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">
+        <Calendar />
+        <Divider my="md" />
+      </AppShell.Navbar>
+
+      <AppShell.Main>
+        <FullCalendar
+          plugins={[ resourceTimeGridPlugin  ]}
+          initialView="resourceTimeGridDay"
+          schedulerLicenseKey="GPL-My-Project-Is-Open-Source"
+          slotDuration={"00:10:00"}
+          slotMinTime={"08:00:00"}
+          slotMaxTime={"20:00:00"}
+          resources={treatment}
+          locale={'ru'}
+          slotLabelFormat={{
+            hour: 'numeric',
+            minute: '2-digit',
+          }}
+          events={events}
+        />
+      </AppShell.Main>
+    </AppShell>
   );
 }
