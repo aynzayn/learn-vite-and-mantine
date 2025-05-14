7822668529876f30806625b9b7afd84fcf4d2621
Зайнуллин Айнур Хакимович <ZaynullinAKh@alrosa.ru>
2025-05-15 03:25:55 +0500

    Добавление плагинов eslint-plugin-simple-import-sort, eslint-plugin-unused-imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,39 @@
 import mantine from 'eslint-config-mantine';
 import tseslint from 'typescript-eslint';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
 
 export default tseslint.config(
   ...mantine,
   { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}', './.storybook/main.ts'] },
   {
-    files: ['**/*.story.tsx'],
-    rules: { 'no-console': 'off' },
+    files: ['**/*.tsx'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': eslintPluginUnusedImports,
+    },
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': 'off',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // Side effect imports.
+            ['^\\u0000'],
+            // Packages. `react` related packages come first.
+            ['^react', '^mobx', '^@?\\w'],
+            // Core-packages
+            ['^@mantine'],
+
+            // Parent imports. Put `..` last.
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            // Other relative imports. Put same-folder imports and `.` last.
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            ['.css$'],
+          ],
+        },
+      ],
+    },
   }
 );
